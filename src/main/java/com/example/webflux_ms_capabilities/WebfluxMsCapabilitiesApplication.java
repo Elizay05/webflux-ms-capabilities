0800af325ca15babefb4ed14757a44c77064d87c
SayiraQuirama <sayira.quirama@pragma.com.co>
2025-03-13 08:40:57 -0500

    Initial configuration

---
+++ src/main/java/com/example/webflux_ms_capabilities/WebfluxMsCapabilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux_ms_capabilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxMsCapabilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxMsCapabilitiesApplication.class, args);
+	}
+
+}
